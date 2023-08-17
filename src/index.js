31bfcf3e7fdad27918461b2a7e3868490f2eff9d
sebastian <sebasfox0510@hotmail.com>
2023-08-17 09:55:14 -0500

    uso de docker y test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const app = require("./app");
 
-app.listen(3000);
-console.log("Server on port", 3000);
 
+async function main() {
+  app.listen(3000);
+  console.log("Server on port", 3000);
+}
+
+main();
